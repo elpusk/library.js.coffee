5479cac95c31cbe8348075c05a3070ab43d272a8
seong-sig <ssyangsnsk@gmail.com>
2025-12-21 12:23:06 +0900

    elpusk.device.usb.vendor.js jsdoc 수정

--- scripts/elpusk/elpusk.device.usb.vendor.js
+++ scripts/elpusk/elpusk.device.usb.vendor.js
@@ -27,50 +27,44 @@
     /**@private */
     var _elpusk = window.elpusk;
 
-    /** documented as elpusk */
     if (!_elpusk) {
         _elpusk = {};
     }
-    /** documented as elpusk.device */
     if (!_elpusk.device) {
         _elpusk.device = {};
     }
-    /** documented as elpusk.device.usb */
     if (!_elpusk.device.usb) {
         _elpusk.device.usb = {};
     }
 
     /**
-     * @class usb vendor defined class
-    */
+     * @class
+     * @classdesc This class is a base class for USB vendor-specific device implementations.
+     */
     if (!_elpusk.device.usb.vendor) {
+        /**
+         * @constructs elpusk.device.usb.vendor
+         * @param {string} s_path The device path.
+         */
         _elpusk.device.usb.vendor = function( s_path ){
             /**
-             * device path
              * @private
+             * @desc The device path.
+             * @type {string}
              */
             this._s_path = s_path;
-
-            /**
-             * @private
-             * @function x
-             * @return {string} device path.
-             */
-
         };
 
         /**
          * @public
-         * @function get_path
-         * @return {string} device path.
+         * @description Retrieves the device path.
+         * @returns {string} The device path.
          */
         _elpusk.device.usb.vendor.prototype.get_path = function()
         {
             return this._s_path;
         }        
     }
-
-
     // the end of function
     window.elpusk = _elpusk;
 }(window));
